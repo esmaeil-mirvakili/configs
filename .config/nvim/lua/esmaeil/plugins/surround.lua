c4735d1ec086869c0ec3c071055692241097a852
esmaeil-mirvakili <smirvaki@ucsc.edu>
2026-03-08 00:48:50 -0800

    nvim added

---
+++ .config/nvim/lua/esmaeil/plugins/surround.lua
@@ -0,0 +1,6 @@
+return {
+  "kylechui/nvim-surround",
+  event = { "BufReadPre", "BufNewFile" },
+  version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+  config = true,
+}
